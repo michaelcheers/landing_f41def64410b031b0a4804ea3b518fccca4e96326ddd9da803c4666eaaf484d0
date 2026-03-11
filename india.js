ee34e7da57841428e7c2b0ae17118ea61f4c883a
Michael Cheers <michaelcheers@users.noreply.github.com>
2026-03-11 14:03:57 -0200

    initial commit

---
+++ india.js
@@ -0,0 +1,7 @@
+(()=>{
+const _=[[6,0],[1,1],[6,5],[5,2],[5,3],[4,5],[3,6],[8,11],[0,10],[3,8],[8,7],[0,2],[0,4],[5,0],[4,1],[8,14],[0,9],[1,11],[7,1],[5,11],[5,12],[1,11],[7,13],[2,9],[5,11]];
+const $='ino@vflmhcsad.g';
+let r='';for(let i=0;i<_.length;i++){r+=$.charAt(_[i][1]);}
+const e=document.getElementById('ob-email');
+if(e){e.href='mailto:'+r;e.textContent=r;}
+})();
